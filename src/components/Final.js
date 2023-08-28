268550c39dc6cd12e16b7301fbfa0517fd4b0917
kalpesh <kalpesh.g@codiste.com>
2023-08-28 18:02:33 +0530

    Changes as per feedback

--- src/components/Final.js
+++ src/components/Final.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { animated, useSpring } from "@react-spring/web";
 import Checked from "../assets/images/checked.svg";
+// import PolygonLogo from "../assets/images/checked.svg";
+import EthLogo from "../assets/images/ethereum-logo.jpg";
+import PolygonLogo from "../assets/images/polygon.png";
+import SolanaLogo from "../assets/images/solana-logo.png";
+import FioLogo from "../assets/images/fiologo-1.png";
 
 function TerminalLogs() {
   const [currentParagraphs, setCurrentParagraphs] = useState([]);
@@ -24,54 +29,127 @@ function TerminalLogs() {
 
   const paragraphs = [
     {
-      text: (vars) => `Registering Domain Name: ${vars.orgName}..`,
+      text: (vars) => `Registering domain name: ${vars.orgName}...`,
       type: "heading",
-      style: { color: "#252525", fontSize: "25px" },
+      style: { color: "#252525", fontSize: "22px" },
     },
     {
-      text: (vars) => `Minting Wallets for: ${vars.user}...`,
+      text: (vars) => `Minting wallets for: ${vars.user}...`,
       type: "heading",
-      style: { color: "#252525", fontSize: "25px" },
+      style: { color: "#252525", fontSize: "22px" },
+      children: [
+        {
+          text: () => "EVM:",
+          style: { color: "#252525 ", fontSize: "16px", fontWeight: "bold" },
+          icon: EthLogo
+        },
+        {
+          text: () => "0x10E0271ec47d55511a047516f2a7301801d55eaB",
+          style: { color: "#252525", fontSize: "16px", marginLeft: "32px", marginBottom: "15px" },
+        },
+        {
+          text: () => "Sola:",
+          style: { color: "#252525", fontSize: "16px", fontWeight: "bold" },
+          icon: SolanaLogo
+        },
+        {
+          text: () => "HN7cABqLq46Es1jh92dQQisAq662SmxELLLsHHe4YWrH",
+          style: { color: "#252525", fontSize: "16px", marginLeft: "32px", marginBottom: "15px" },
+        },
+        {
+          text: () => "Polygon:",
+          style: { color: "#252525", fontSize: "16px", fontWeight: "bold" },
+          icon: PolygonLogo
+        },
+        {
+          text: () => "0xF2A1246e60a57c899DCD6e5166e246bc5cd7E783",
+          style: { color: "#252525", fontSize: "16px", marginLeft: "32px", marginBottom: "15px" },
+        },
+        {
+          text: () => "Fio:",
+          style: { color: "#252525", fontSize: "16px", fontWeight: "bold" },
+          icon: FioLogo
+        },
+        {
+          text: () => "0xF2A1246e60a57c899DCD6e5166e246bc5cd7E783",
+          style: { color: "#252525", fontSize: "16px", marginLeft: "32px" },
+        },
+      ],
     },
     {
-      text: (vars) => "EVM",
-      style: { color: "#252525 ", fontSize: "20px", fontWeight: "bold" },
-    },
-    {
-      text: (vars) => "Ox123s ... 223aSf ..",
-      style: { color: "#252525", fontSize: "20px" },
-    },
-    { text: (vars) => "Sola..", style: { color: "#252525", fontSize: "20px" } },
-    {
-      text: (vars) => `Registering domain name ${vars.user}@${vars.orgName}...`,
+      text: (vars) => `Registering handle name ${vars.user}@${vars.orgName}...`,
       type: "heading",
       style: { color: "#252525", fontSize: "25px" },
     },
   ];
 
-  useEffect(() => {
-    const currentText = paragraphs[currentParagraphIndex]?.text(data);
-    if (currentParagraphIndex < paragraphs.length && currentText) {
-      if (currentCharIndex < currentText.length) {
-        setTimeout(() => {
-          const updatedParagraphs = [...currentParagraphs];
-          if (updatedParagraphs[currentParagraphIndex]) {
-            updatedParagraphs[currentParagraphIndex].text +=
-              currentText[currentCharIndex];
-          } else {
-            updatedParagraphs[currentParagraphIndex] = {
-              text: currentText[currentCharIndex],
-              style: paragraphs[currentParagraphIndex].style,
-              type: paragraphs[currentParagraphIndex].type,
-            };
-          }
-          setCurrentParagraphs(updatedParagraphs);
-          setCurrentCharIndex(currentCharIndex + 1);
-        }, 50);
-      } else {
-        setCurrentCharIndex(0);
-        setCurrentParagraphIndex(currentParagraphIndex + 1);
+  const getCurrentText = () => {
+    const parentParagraph = paragraphs[currentParagraphIndex];
+    if (!parentParagraph) return "";
+
+    const parentText = parentParagraph?.text(data);
+    if (currentCharIndex < parentText?.length) {
+      return parentText[currentCharIndex];
+    }
+
+    let totalChars = parentText?.length;
+    for (let i = 0; i < parentParagraph?.children?.length; i++) {
+      const child = parentParagraph?.children[i];
+      if (currentCharIndex < totalChars + child?.text(data)?.length) {
+        return child?.text(data)[currentCharIndex - totalChars];
       }
+      totalChars += child?.text(data)?.length;
+    }
+
+    return "";
+  }
+
+  useEffect(() => {
+    const currentTextChar = getCurrentText();
+    if (currentParagraphIndex < paragraphs?.length && currentTextChar) {
+      setTimeout(() => {
+        const updatedParagraphs = [...currentParagraphs];
+        const parentParagraph = paragraphs[currentParagraphIndex];
+        const parentText = parentParagraph?.text(data);
+
+        if (currentCharIndex < parentText?.length) {
+          // Append character to parent
+          if (updatedParagraphs[currentParagraphIndex]) {
+            updatedParagraphs[currentParagraphIndex].text += currentTextChar;
+          } else {
+            updatedParagraphs.push({
+              ...parentParagraph,
+              text: currentTextChar,
+              children: [],
+            });
+          }
+        } else {
+          // Append character to one of the children
+          let totalChars = parentText?.length;
+          for (let i = 0; i < parentParagraph?.children?.length; i++) {
+            const child = parentParagraph?.children[i];
+            if (currentCharIndex < totalChars + child?.text(data).length) {
+              if (!updatedParagraphs[currentParagraphIndex]?.children[i]) {
+                updatedParagraphs[currentParagraphIndex].children[i] = {
+                  ...child,
+                  text: currentTextChar,
+                };
+              } else {
+                updatedParagraphs[currentParagraphIndex].children[i].text +=
+                  currentTextChar;
+              }
+              break;
+            }
+            totalChars += child?.text(data)?.length;
+          }
+        }
+
+        setCurrentParagraphs(updatedParagraphs);
+        setCurrentCharIndex(currentCharIndex + 1);
+      }, 50);
+    } else {
+      setCurrentCharIndex(0);
+      setCurrentParagraphIndex(currentParagraphIndex + 1);
     }
   }, [
     currentCharIndex,
@@ -95,25 +173,47 @@ function TerminalLogs() {
     }
   };
 
+  const getLoaderOrCheckedIcon = (paragraph, index) => {
+    if (paragraph.type === "heading") {
+      return index === currentParagraphIndex ? (
+        <span
+          style={{
+            ...getStyle(paragraph),
+            marginTop: getStyle(paragraph)?.marginTop + 5,
+          }}
+        >
+          <span className="loader"></span>
+        </span>
+      ) : index < currentParagraphIndex ? (
+        <span
+          style={{
+            ...getStyle(paragraph),
+            marginTop: getStyle(paragraph)?.marginTop + 5,
+          }}
+        >
+          <img src={Checked} alt="Checked icon" />
+        </span>
+      ) : null;
+    } else {
+      return <div className="w-[34px]"></div>;
+    }
+  }
+
   return (
-    <div className="!lg:mt-[-300px] mt-[-150px] px-6">
+    <div className="!lg:mt-[-200px] lg:ml-32 mt-[-100px] px-6 lg:w-[700px]">
       {currentParagraphs.map((paragraph, index) => (
-        <div key={index} className="flex  space-x-2">
-          {paragraph.type === "heading" && index < currentParagraphIndex ? (
-            <span
-              style={{
-                ...getStyle(paragraph),
-                marginTop: getStyle(paragraph)?.marginTop + 5,
-              }}
-            >
-              <img src={Checked} />
-            </span>
-          ) : (
-            <div className="w-[34px]"></div>
-          )}
-          <span style={getStyle(paragraph)}>{paragraph.text}</span>
-          {index === currentParagraphIndex && <span></span>}
-          <br />
+        <div key={index}>
+          <div className="flex space-x-2">
+            {getLoaderOrCheckedIcon(paragraph, index)}
+            <span style={getStyle(paragraph)}>{paragraph?.text}</span>
+          </div>
+          {paragraph.children?.map((child, childIndex) => (
+            <div key={childIndex} className="flex space-x-2 ml-4">
+              {getLoaderOrCheckedIcon(child, childIndex)}
+              { child?.icon ? <img src={child?.icon} className="w-4 h-4 mt-1 mix-blend-multiply	" /> : null}
+              <span className="w-[250px] inline-block break-words sm:w-full" style={getStyle(child)}>{child?.text}</span>
+            </div>
+          ))}
         </div>
       ))}
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,22 @@ import { useSpring, animated, useTrail } from "@react-spring/web";
 import React, { useEffect, useState } from "react";
 import edit from "../assets/images/editMode.svg";
 
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
 const List = ({ setCurrentScreen }) => {
   const [items, setItems] = useState([
     "",
@@ -10,35 +26,43 @@ const List = ({ setCurrentScreen }) => {
     "EOS23056t",
   ]);
   const org = localStorage.getItem("org");
-  const _user = localStorage.getItem("user");
+  const _user = localStorage.getItem("userName");
 
   const [currentOrg, setCurrentOrg] = useState("");
+
+  const debounceSearch = useDebounce(items[0], 800);
 
   useEffect(() => {
     setCurrentOrg(org);
   }, [org]);
 
   useEffect(() => {
-    if(_user) {
+    if (_user) {
       const arr = [...items];
       arr[0] = _user;
       setItems(arr);
     }
-  }, [_user])
+  }, [_user]);
 
   const handleSave = () => {
+    const userHandle = items[0]?.split('@')?.[0];
     setCurrentScreen(4);
-    localStorage.setItem("user", items[0]);
+    localStorage.setItem("user", `${userHandle}`);
+    localStorage.setItem("userName", items[0]);
   };
 
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
   const animations = useTrail(items.length, {
-    transform: items[0]?.length > 3 ? "translateY(0px)" : "translateY(100px)",
-    opacity: items[0]?.length > 3 ? 1 : 0,
+    transform:
+      debounceSearch?.length > 3 && mailformat.test(debounceSearch)
+        ? "translateY(0px)"
+        : "translateY(100px)",
+    opacity:
+      debounceSearch?.length > 3 && mailformat.test(debounceSearch) ? 1 : 0,
     from: { transform: "translateY(100px)" },
     trail: 200,
   });
-
-  console.log(items)
 
   return (
     <div className="mt-[-40px]">
@@ -58,30 +82,36 @@ const List = ({ setCurrentScreen }) => {
         </div>
 
         <div className="space-y-4">
-          {animations.map((props, index) => (
-            <animated.div
-              key={index}
-              className="flex p-4 px-6 w-[345px] h-[54px] rounded-xl bg-[#262626] items-center justify-between"
-              style={{
-                ...props,
-              }}
-            >
-              <div className="font-bold text-[16px] text-white">
-                {index === 0 ? `${items[0]}@${currentOrg}` : items[index]}
-              </div>
-              <div className="w-1/2 text-right" onClick={() => {
-                const arr = [...items];
-                arr[0] = items[index]
-                setItems(arr);
-              }}>
-                <img className="w-[28px] ml-4 inline" src={edit} />
-              </div>
-            </animated.div>
-          ))}
+          {animations.map((props, index) => {
+            const userHandle = items[0]?.split('@')?.[0];
+            return (
+              <animated.div
+                key={index}
+                className="flex p-4 px-6 w-[345px] h-[54px] rounded-xl bg-[#262626] items-center justify-between"
+                style={{
+                  ...props,
+                }}
+              >
+                <div className="font-bold text-[16px] text-white">
+                  {index === 0 ? `${userHandle}@${currentOrg}` : items[index]}
+                </div>
+                <div
+                  className="w-1/2 text-right"
+                  onClick={() => {
+                    const arr = [...items];
+                    arr[0] = items[index];
+                    setItems(arr);
+                  }}
+                >
+                  <img className="w-[28px] ml-4 inline" src={edit} />
+                </div>
+              </animated.div>
+            );
+          })}
         </div>
 
         <div>
-          {items[0]?.length > 3 ? (
+          {debounceSearch?.length > 3 ? (
             <button
               onClick={handleSave}
               className={`countinue-btn mx-auto hidden`}
