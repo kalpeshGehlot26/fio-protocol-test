76c821ec8b47cd89f12dd1f8a326922bd4a1db23
kalpesh <kalpesh.g@codiste.com>
2023-09-01 11:36:16 +0530

    Text streaming animation on email page

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,7 @@
-import { useSpring, animated, useTrail } from "@react-spring/web";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Checked from "../assets/images/checked.svg";
-import edit from "../assets/images/editMode.svg";
 import EthLogo from "../assets/images/ethereum-logo.jpg";
 import PolygonLogo from "../assets/images/polygon.png";
-import SolanaLogo from "../assets/images/solana-logo.png";
 import EOSLogo from "../assets/images/eos-coin.png";
 
 const useDebounce = (value, delay) => {
@@ -25,34 +22,24 @@ const useDebounce = (value, delay) => {
 
 const List = ({ setCurrentScreen }) => {
   const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [startAnimation, setStartAnimation] = useState(false);
+  const [currentOrg, setCurrentOrg] = useState("");
+  const [displayedChars, setDisplayedChars] = useState([]);
+
   const org = localStorage.getItem("org");
   const _user = localStorage.getItem("user");
   const _email = localStorage.getItem("userName");
 
-  const [currentOrg, setCurrentOrg] = useState("");
-
-  const initialSentences = ["", "Ox123 ... s223aS", "Abeog$/", "EOS23056t"];
-  const walletIcons = [EthLogo, PolygonLogo, EOSLogo, ]
+  const initialSentences = ["Ox123 ... s223aS", "Abeog$/", "EOS23056t"];
+  const walletIcons = [EthLogo, PolygonLogo, EOSLogo];
 
   const [sentences, setSentences] = useState(initialSentences);
   const [userHandle, setUserHandle] = useState("");
   const [loadedSentences, setLoadedSentences] = useState([]);
 
   const debounceSearch = useDebounce(email, 800);
-
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-  useEffect(() => {
-    if (mailformat.test(debounceSearch)) {
-      const _userHandle = debounceSearch?.split("@")?.[0];
-      setUserHandle(_userHandle);
-      sentences.forEach((_, index) => {
-        setTimeout(() => {
-          setLoadedSentences((prevState) => [...prevState, index]);
-        }, (index + 1) * 5000);
-      });
-    }
-  }, [sentences, debounceSearch]);
+  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   useEffect(() => {
     setCurrentOrg(org);
@@ -61,11 +48,57 @@ const List = ({ setCurrentScreen }) => {
   useEffect(() => {
     if (_user) {
       setUserHandle(_user);
+      setStartAnimation(true);
     }
     if (_email) {
       setEmail(_email);
     }
   }, [_user, _email]);
+
+  const handleInputChange = (e) => {
+    setUserHandle(e.target.value);
+  };
+
+  const handleStartAnimation = () => {
+    if (mailformat.test(email)) {
+      setIsEmailValid(true);
+      setStartAnimation(true);
+    } else {
+      setIsEmailValid(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!startAnimation) return;
+
+    const typeSentence = (sentence, index) => {
+      let charCount = 0;
+
+      const typeInterval = setInterval(() => {
+        setDisplayedChars((prevChars) => {
+          const newChars = [...prevChars];
+          newChars[index] = sentence.substring(0, charCount);
+          return newChars;
+        });
+        charCount++;
+
+        if (charCount > sentence.length) {
+          clearInterval(typeInterval);
+          setLoadedSentences((prevState) => [...prevState, index]);
+
+          if (index < sentences.length) {
+            typeSentence(sentences[index], index + 1);
+          }
+        }
+      }, 100);
+    };
+
+    if (mailformat.test(debounceSearch)) {
+      const _userHandle = debounceSearch?.split("@")?.[0];
+      setUserHandle(_userHandle);
+      typeSentence(`${_userHandle}@${currentOrg}`, 0);
+    }
+  }, [sentences, debounceSearch, currentOrg, startAnimation]);
 
   const handleSave = () => {
     setCurrentScreen(4);
@@ -73,11 +106,12 @@ const List = ({ setCurrentScreen }) => {
     localStorage.setItem("userName", email);
   };
 
-  const handleInputChange = (e) => {
-    setUserHandle(e.target.value);
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setStartAnimation(false);
+    setLoadedSentences([]); // Reset loaded sentences
+    setDisplayedChars([]); // Reset displayed characters
   };
-
-  const inputRef = useRef(null);
 
   return (
     <div className="mt-[-40px]">
@@ -87,13 +121,23 @@ const List = ({ setCurrentScreen }) => {
             placeholder="naga@metakeep.xyz"
             value={email}
             className="input-domain"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleEmailChange}
           />
-          <span className="return-btn absolute top-[30%] right-2">{`->`}</span>
+          <span
+            className="return-btn cursor-pointer absolute top-[30%] right-2"
+            onClick={handleStartAnimation}
+          >{`->`}</span>
         </div>
+        {!isEmailValid && (
+          <span style={{ color: "red", display: "block", marginTop: "0px" }}>
+            Invalid email address.
+          </span>
+        )}
 
         <div className="min-h-[200px] w-full">
-          {debounceSearch?.length > 3 && mailformat.test(debounceSearch) ? (
+          {debounceSearch?.length > 3 &&
+          mailformat.test(debounceSearch) &&
+          startAnimation ? (
             <div className="App font-Inter">
               <div className="w-full text-center ">
                 <div className="sentence-first mb-8 mx-auto">
@@ -106,12 +150,9 @@ const List = ({ setCurrentScreen }) => {
                   ) : (
                     <div className="loader-org w-2 mt-1 mr-2"></div>
                   )}
-                  {!loadedSentences.includes(0) ? (
-                    <div className="editableSentence">{`${userHandle}@${currentOrg}`}</div>
-                  ) : (
+                  {loadedSentences.includes(0) ? (
                     <>
                       <input
-                        ref={inputRef}
                         value={userHandle}
                         onChange={handleInputChange}
                         className="editableSentence"
@@ -120,55 +161,40 @@ const List = ({ setCurrentScreen }) => {
                       />
                       <div className="editableSentence">{`@${currentOrg}`}</div>
                     </>
+                  ) : (
+                    <div className="editableSentence">
+                      {displayedChars[0] || ""}
+                    </div>
                   )}
                 </div>
               </div>
-              {sentences.slice(1).map((sentence, index) => (
-                <div key={index} className="sentence my-2 text-[18px]">
-                  {loadedSentences.includes(index + 1) ? (
-                    <img src={Checked} className="w-4" alt="Checked icon" />
-                  ) : (
-                    <div className="loader-org w-2"></div>
-                  )}
-                  <img src={walletIcons[index]} className="w-4 h-4 mix-blend-multiply	" /> 
-                  {sentence}
-                </div>
-              ))}
+              {loadedSentences.includes(0) &&
+                sentences.map((sentence, index) => (
+                  <div key={index} className="sentence my-2 text-[18px]">
+                    {loadedSentences.includes(index + 1) ? (
+                      <img src={Checked} className="w-4" alt="Checked icon" />
+                    ) : (
+                      (index === 0 || loadedSentences.includes(index)) && (
+                        <div className="loader-org w-2"></div>
+                      )
+                    )}
+                    {(index === 0 || loadedSentences.includes(index)) && (
+                      <img
+                        src={walletIcons[index]}
+                        className="w-4 h-4 mix-blend-multiply"
+                      />
+                    )}
+                    {displayedChars[index + 1] || ""}
+                  </div>
+                ))}
             </div>
           ) : null}
         </div>
 
-        {/* <div className="space-y-4">
-          {animations.map((props, index) => {
-            const userHandle = items[0]?.split('@')?.[0];
-            return (
-              <animated.div
-                key={index}
-                className="flex p-4 px-6 w-[345px] h-[54px] rounded-xl bg-[#262626] items-center justify-between"
-                style={{
-                  ...props,
-                }}
-              >
-                <div className="font-bold text-[16px] text-white">
-                  {index === 0 ? `${userHandle}@${currentOrg}` : items[index]}
-                </div>
-                <div
-                  className="w-1/2 text-right"
-                  onClick={() => {
-                    const arr = [...items];
-                    arr[0] = items[index];
-                    setItems(arr);
-                  }}
-                >
-                  <img className="w-[28px] ml-4 inline" src={edit} />
-                </div>
-              </animated.div>
-            );
-          })}
-        </div> */}
-
         <div>
-          {debounceSearch?.length > 3 && mailformat.test(debounceSearch) && loadedSentences?.length === sentences?.length ? (
+          {debounceSearch?.length > 3 &&
+          mailformat.test(debounceSearch) &&
+          loadedSentences?.length === sentences?.length + 1 ? (
             <button
               onClick={handleSave}
               className={`countinue-btn mx-auto hidden`}
